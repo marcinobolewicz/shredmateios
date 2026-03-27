1ca86c121d97f34f3fb95bbb140775b8af77df2b
Marcin Obolewicz <obolewicz.m@gmail.com>
2026-03-27 01:50:49 +0100

    In app notificatio fix

--- Packages/App/Sources/RootView.swift
+++ Packages/App/Sources/RootView.swift
@@ -56,15 +56,8 @@ public struct RootView: View {
             async let _ = dependencies.sportsService.fetchSports()
             await authState.restoreSession()
             router.flow = authState.isLoggedIn ? .user : .guest
-
-            if authState.isLoggedIn {
-                await connectChat()
-                await PushNotificationsBridge.requestAuthorizationAfterLogin()
-            }
         }
-        .onChange(of: authState.isLoggedIn) { _, isLoggedIn in
-            router.flow = isLoggedIn ? .user : .guest
-
+        .onChange(of: authState.isLoggedIn, initial: true) { _, isLoggedIn in
             if isLoggedIn {
                 Task {
                     await connectChat()
